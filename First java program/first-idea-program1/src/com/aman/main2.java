6220c6cf819ddd0bd978f3dd6bd61519e788deef
aman14345 <aman425singh@gmail.com>
2022-10-08 12:38:51 +0530

    main2

---
+++ First java program/first-idea-program1/src/com/aman/main2.java
@@ -0,0 +1,10 @@
+package com.aman;
+import java.util.Scanner;
+
+public class main2 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println(input.nextLine());
+
+    }
+}
