2c38a720abeb872fd595137a32096e6dde79c220
aman14345 <aman425singh@gmail.com>
2022-10-08 13:15:08 +0530

    input integer

---
+++ First java program/First Java Lecture/src/com/aman/Main3.java
@@ -0,0 +1,9 @@
+package com.aman;
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println(input.nextInt());
+    }
+}
